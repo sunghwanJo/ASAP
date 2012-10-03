2b600d7cf4674d50ac08de5f047b7dfb8168c531
Acuros <acuros@MacBookAir>
2012-10-03 23:07:43 +0900

    * Model with sqlarchemy added

--- src/ASAP/settings.py
+++ src/ASAP/settings.py
@@ -7,3 +7,12 @@ DEBUG = True
 
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 LOG_FILE_PATH = os.path.join(PROJECT_PATH, 'log', 'log.txt')
+
+DATABASE = {
+    'type':'sqlite',#sqlite or mysql or other
+    'connection_string':'',#if type is other, requires custom connection string
+    'dbname':'db.db',#database name for mysql, filename for sqlite
+    'username':'',#for mysql
+    'password':'',#for mysql
+    'host':'',#for mysql
+}
