293572eec3782af8ebe3aaad2feb2938efd7dc3a
Acuros <acuros@MacBookAir>
2012-10-11 01:13:50 +0900

    * views.py contents are removed

--- views.py
+++
@@ -1,7 +0,0 @@
-from ASAP.models import *
-
-def get_university_list(request):
-    database_manager = DatabaseManager()
-    session = database_manager.get_session()
-    universities = session.query(University).order_by(University.name)
-    return dict(university_list=[university.to_dict() for university in universities])
